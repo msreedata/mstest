07b8af44dbbb5a35329a7ccfbdbc3ee81adae027
Sreejith Mani <Sreejith.Mani@gmail.com>
2018-06-26 11:12:30 +0200

    s

---
+++ rest-api-dotnet/Controllers/v2/FoodsController.cs
@@ -0,0 +1,15 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace SampleWebApiAspNetCore.v2.Controllers
+{
+    [ApiVersion("2.0")]
+    [Route("api/v{version:apiVersion}/[controller]")]
+    public class FoodsController : ControllerBase
+    {
+        [HttpGet]
+        public ActionResult Get()
+        {
+            return Ok("2.3.15");
+        }
+    }
+}
---
+++ rest-api-dotnet/Tests/UnitTests.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ProductAPI
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
